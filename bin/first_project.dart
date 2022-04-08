bc9f9c479254c708fc940ee3587568bcbb8b37ef
Victorer <victor@myicellar.com>
2022-04-08 10:09:04 +0900

    038_format_code_in_vscode

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -1,60 +1,21 @@
-import 'dart:math';
-
-enum RockScissorPaper{
-  rock,scissors,paper
-}
 void main() {
-  //0 == ROCK
-  //1 == SISSOR
-  //2 == PAPER
-  int random = Random().nextInt(3); 
-  RockScissorPaper computer = RockScissorPaper.rock;
+  myNameInSentence('yang', 'Victor', middleName: 'victorianoch', age3: 18);
+}
 
-  switch(random){
-    case 0:
-      computer = RockScissorPaper.rock;
-      break;
-    case 1:
-      computer = RockScissorPaper.scissors;
-      break;
-    case 2:
-      computer = RockScissorPaper.paper;
-      break;
+myNameInSentence(
+  String lastName,
+  String givenName, {
+  String? middleName,
+  int? age,
+  int? age1,
+  int? age2,
+  int? age3,
+  int? age4,
+  int? age5,
+}) {
+  if (middleName == null) {
+    print("My name is $givenName $lastName");
+  } else {
+    print("My name is $givenName $middleName $lastName $age3");
   }
-
-  const RockScissorPaper me = RockScissorPaper.paper;
-
-  switch(computer){
-    case RockScissorPaper.rock:
-      if(me == RockScissorPaper.paper){
-        print('I win!!');
-      }else if(me == RockScissorPaper.scissors){
-        print('I lost!!');
-      }else{
-        print('draw');
-      }
-      break;
-    case RockScissorPaper.scissors:
-      if(me == RockScissorPaper.paper){
-        print('I lost!!');
-      }else if(me == RockScissorPaper.scissors){
-        print('draw');
-      }else{
-        print('I win!!');
-      }
-      break;
-    case RockScissorPaper.paper:
-      if(me == RockScissorPaper.paper){
-        print('draw');
-      }else if(me == RockScissorPaper.scissors){
-        print('I win!!');
-      }else{
-        print('I lost!!');
-      }
-      break;
-  }
-
-
-
-  print('$me - $computer');
 }
